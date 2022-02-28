d463b3a127ff636f010e2fe71f86318567a83966
NLBSRBIJA\teodora.ilic <teodora.ilic@nlb.rs>
2022-02-28 17:55:51 +0100

    first commit

---
+++ src/test/java/master/springframework/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package master.springframework.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
